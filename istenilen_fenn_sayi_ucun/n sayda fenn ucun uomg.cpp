73590c8f5b29e7dac31e56b267761eada633cbaa
Javid Memmedkhanli <145150258+Javid0800@users.noreply.github.com>
2025-05-04 21:48:25 +0400

    n sayda fenn ucun uomg.cpp

--- istenilen_fenn_sayi_ucun/n sayda fenn ucun uomg.cpp
+++ istenilen_fenn_sayi_ucun/n sayda fenn ucun uomg.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    double eded1, eded2;
-    char hesap;
+int main()
+{
+    int n;
+    double cavab, cavab_suret=0, cavab_mexrec=0;
 
-    cout<<"1. Ededi yazin: ";
-    cin>>eded1;
-    cout<<"2. Ededi yazin: ";
-    cin>>eded2;
-    cout<<"hesap (+, -, *, /): ";
-    cin>>hesap;
+    cout<<"Derslerin sayini daxil edin: ";
+    cin>>n;
 
-    switch(hesap)
+    for(int i=1; i<=n; i++)
     {
-        case '+':
-            cout<<"Cavab: "<<eded1+eded2;
-            break;
-        case '-':
-            cout<<"Cavab: "<<eded1-eded2;
-            break;
-        case '*':
-            cout<<"Cavab: "<<eded1*eded2;
-            break;
-        case '/':
-            if(eded2!=0)
-                cout<<"Cavab: "<<eded1/eded2;
-            else
-                cout << "Xeta: Sifira bolunme yoxdur!";
-            break;
-        default:
-            cout << "Xeta!";
+        double bal, kredit;
+        cout<<i<<". Fennin Umumi Bali ve Krediti: ";
+        cin>>bal>>kredit;
+        cavab_suret+=bal*kredit;
+        cavab_mexrec+=kredit;
     }
+
+    cavab=cavab_suret/cavab_mexrec;
+    cout<<"Ortalama: "<<cavab;
+
     return 0;
 }
